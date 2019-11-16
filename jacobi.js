d78b4d086fa391df7d0a9cefab2bddc937c7c25f
Arkajit Mandal <arkajitmandal@gmail.com>
2019-11-16 17:25:17 -0500

    Givens Matrix done

--- jacobi.js
+++ jacobi.js
@@ -8,11 +8,17 @@ var Rij = function(i,j,theta,N){
     for (var i = 0; i<N;i++){
         Mat[i] = Array(N) 
     }
-    // Put 1 everywhere
+    // Identity Matrix
     for (var i = 0; i<N;i++){
         for (var j = 0; i<N;i++){
-            Mat[i][j] = (i===j)*1.0
+            Mat[i][j] = (i===j)*1.0;
         }
     }
+    Rij = Rot(theta);
+    // Put Rotation part in i, j
+    Mat[i][i] = Rij[0][0] // 11
+    Mat[j][j] = Rij[1][1] // 22
+    Mat[i][j] = Rij[0][1] // 12
+    Mat[j][i] = Rij[1][0] // 21
     return Mat
 }
