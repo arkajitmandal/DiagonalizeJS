ba54c99e74166c807968fde27f4eb541f1542861
Arkajit Mandal <arkajitmandal@gmail.com>
2019-11-16 17:15:20 -0500

    Givens Matrix

--- jacobi.js
+++ jacobi.js
@@ -1,4 +1,12 @@
-let Rot = function(theta){
+var Rot = function(theta){
     Mat = [[Math.cos(theta),Math.sin(theta)],[-Math.sin(theta),Math.cos(theta)]];
+    return Mat
+}
+
+var Rij = function(i,j,theta,N){
+    Mat = Array(N) 
+    for (var i = 0; i<N;i++){
+        Mat[i] = Array(N) 
+    }
     return Mat
 }
