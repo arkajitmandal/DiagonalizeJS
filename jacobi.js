1c1f7c661ab21766ff89713a27ce4860f57aac14
Arkajit Mandal <arkajitmandal@gmail.com>
2019-11-16 17:21:41 -0500

    Givens Matrix update

--- jacobi.js
+++ jacobi.js
@@ -8,5 +8,11 @@ var Rij = function(i,j,theta,N){
     for (var i = 0; i<N;i++){
         Mat[i] = Array(N) 
     }
+    // Put 1 everywhere
+    for (var i = 0; i<N;i++){
+        for (var j = 0; i<N;i++){
+            Mat[i][j] = (i===j)*1.0
+        }
+    }
     return Mat
 }
